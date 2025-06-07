f84bb0aa10e516e96588fd33c646731d6234b540
Luc654 <lucas.dieleman@outlook.com>
2025-06-07 22:44:45 +0200

    Feat: Basic webserver initialization

--- rotorcraft.ino
+++ rotorcraft.ino
@@ -1,9 +1,66 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
 
+
+// -------------------- Wifi Setup --------------------
+const char* ssid = "rotorcraft";
+const char* pass = "password123";
+
+WiFiServer server(80);
+String header;
+
+
+void enableServer() {
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid, pass);
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+  server.begin(); 
+}
+
+bool waitConnection(){
+  bool conn = false;
+  while (!conn){
+    if(WiFiClient client = server.available()){
+      conn = true;
+    }
+  }
+  int sequence[] = {20, 50, 20, 50, 50, 50};
+  flash(sequence, 6);
+}
+
+// -------------------- Led Setup --------------------
+
+int line = 26;
+
+
+void flash(int sequence[], int length) {
+  for (int i = 0; i < length; i = i + 2) {
+    digitalWrite(line, HIGH);
+    delay(sequence[i]);
+    digitalWrite(line, LOW);
+    delay(sequence[i+1]);
+  }
+}
+void initLed(){
+  pinMode(line, OUTPUT);
+  int sequence[] = {100, 200, 100, 200};
+  flash(sequence, 4);
+
+}
+
+
+
+
+void setup() {
+  Serial.begin(115200);
+  initLed();
+  enableServer();
+  waitConnection();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
+
+
